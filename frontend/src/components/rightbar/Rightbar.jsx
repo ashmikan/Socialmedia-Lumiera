84f076f41ff27a30b739f896d229dd632763db54
ashmikan <ashmika.nathali123@gmail.com>
2025-12-05 10:27:02 +0530

    Add Rightbar component - user suggestions

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -2,7 +2,34 @@ import "./Rightbar.scss"
 
 const Rightbar = () => {
   return (
-    <div className="rightbar"> Rightbar</div>
+    <div className="rightbar">
+        <div className="container">
+            <div className="item">
+                <span>Suggestions For You</span>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <span>Jane</span>
+                    </div>
+                    <div className="buttons">
+                        <button>Follow</button>
+                        <button>Dismiss</button> 
+                    </div>   
+                </div>
+
+                <div className="user">
+                    <div className="userInfo">
+                        <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <span>John</span>
+                    </div>
+                    <div className="buttons">
+                        <button>Follow</button>
+                        <button>Dismiss</button> 
+                    </div>   
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
