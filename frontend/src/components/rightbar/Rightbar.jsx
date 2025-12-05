d255aa6eb8ca95ee84fd0ba90a3b393299793a38
ashmikan <ashmika.nathali123@gmail.com>
2025-12-05 11:37:05 +0530

    Add latest activities section to Rightbar

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -28,6 +28,49 @@ const Rightbar = () => {
                     </div>   
                 </div>
             </div>
+
+            <div className="item">
+                <span>Latest Activities</span>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <p>
+                            <span>Jane</span> changed cover picture
+                        </p>
+                    </div>
+                    <span>1 min ago</span>   
+                </div>
+
+                <div className="user">
+                    <div className="userInfo">
+                        <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <p>
+                            <span>John</span> liked a post
+                        </p>
+                    </div>
+                    <span>2 min ago</span>      
+                </div>
+
+                <div className="user">
+                    <div className="userInfo">
+                        <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <p>
+                            <span>Leo</span> liked a comment
+                        </p>
+                    </div>
+                    <span>5 min ago</span>      
+                </div>
+
+                <div className="user">
+                    <div className="userInfo">
+                        <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <p>
+                            <span>Peter</span> shared a post
+                        </p>
+                    </div>
+                    <span>5 min ago</span>      
+                </div>
+            </div>
         </div>
     </div>
   )
