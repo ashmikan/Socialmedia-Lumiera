83c5485a7ea8ad39b2a5922c3fd06d89e1d6c936
ashmikan <ashmika.nathali123@gmail.com>
2025-12-06 20:25:04 +0530

    Add Posts components

--- frontend/src/components/stories/Stories.jsx
+++ frontend/src/components/stories/Stories.jsx
@@ -40,7 +40,7 @@ const Stories = () => {
                 <button>+</button>
             </div>
         {stories.map(story=>(
-            <div className="story">
+            <div className="story" key={story.id}>
                 <img src={story.img} alt=""/>
                 <span>{story.name}</span>
             </div>
