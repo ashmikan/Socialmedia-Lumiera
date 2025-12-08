5c6b49df20732cde2023579b404f2a7ee408b17b
ashmikan <ashmika.nathali123@gmail.com>
2025-12-08 17:50:22 +0530

    Implement API routes and controllers

---
+++ api/controllers/auth.js
@@ -0,0 +1,17 @@
+export const register = (req, res) => {
+    //CHECK IF USER EXISTS
+
+    //CREATE NEW USER
+        //HASH THE PASSWORD
+
+} 
+
+export const login = (req, res) => {
+    //TODO
+
+}
+
+export const logout = (req, res) => {
+    //TODO
+
+}
