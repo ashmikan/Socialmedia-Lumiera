4bc924af2402c2d4dbce890a44c1f0da231759c7
ashmikan <ashmika.nathali123@gmail.com>
2025-12-10 17:40:56 +0530

    remove deprecated packages and update dependencies

---
+++ api/controllers/like.js
@@ -0,0 +1,14 @@
+export const getLikes = (req, res) => {
+  // TODO: Implement get likes logic
+  res.status(200).json({ message: "Get likes endpoint" });
+};
+
+export const addLike = (req, res) => {
+  // TODO: Implement add like logic
+  res.status(200).json({ message: "Add like endpoint" });
+};
+
+export const deleteLike = (req, res) => {
+  // TODO: Implement delete like logic
+  res.status(200).json({ message: "Delete like endpoint" });
+};
