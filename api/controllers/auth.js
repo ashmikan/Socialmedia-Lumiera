4e60867aad0c73e8f5f022977be0b293a3ffec34
ashmikan <ashmika.nathali123@gmail.com>
2025-12-10 16:24:54 +0530

    Implement registration functionality with axios

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -50,6 +50,9 @@ export const login = (req, res) => {
 }
 
 export const logout = (req, res) => {
-    //TODO
+    res.clearCookie("access_token", {
+        secure: true,
+        sameSite: "none"
+    }).status(200).json("User has been logged out.");
 
 }
