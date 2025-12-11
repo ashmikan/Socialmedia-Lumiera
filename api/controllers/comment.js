003434bdb869910d995e419a1c33a78fe188e46b
ashmikan <ashmika.nathali123@gmail.com>
2025-12-11 21:28:04 +0530

    implement getComments functionality

--- api/controllers/comment.js
+++ api/controllers/comment.js
@@ -1,9 +1,14 @@
+import { db } from '../connect.js';
+
 export const getComments = (req, res) => {
-  // TODO: Implement get comments logic
-  res.status(200).json({ message: "Get comments endpoint" });
+   const q = `SELECT c.*, u.id AS userId, name, profilePic FROM comments AS c JOIN users AS u ON (u.id = c.userId)
+    WHERE c.postId = ? ORDER BY c.createdAt DESC
+    `;
+
+  db.query(q, [req.query.postId], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json(data);
+  });
 };
 
-export const addComment = (req, res) => {
-  // TODO: Implement add comment logic
-  res.status(200).json({ message: "Add comment endpoint" });
-};
+
--- frontend/src/components/comments/Comments.jsx
+++ frontend/src/components/comments/Comments.jsx
@@ -1,31 +1,20 @@
 import "./Comments.scss"
 import { useContext } from "react";
 import { AuthContext } from "../../context/authContext";
+import { useQuery } from '@tanstack/react-query';
+import { makeRequest } from "../../axios";
+import moment from "moment";
 
-const Comments = () => {
-
+const Comments = ({postId}) => {
   const { currentUser } = useContext(AuthContext);
 
-  //Temporary
-  const comments = [
-    {
-      id: 1,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-      name: "John Doe",
-      userId: 1,
-      profilePicture:
-        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      id: 2,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-      name: "Jane Doe",
-      userId: 2,
-      profilePicture:
-        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    },
-  ];
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ["comments", postId],
+    queryFn: () =>
+      makeRequest.get(`/comments?postId=${postId}`).then((res) => res.data),
+  });
   
+  console.log(data);
   
   return (
     <div className="comments">
@@ -34,16 +23,20 @@ const Comments = () => {
             <input type="text" placeholder="Write a comment..."/>
             <button>Send</button>
         </div>
-        {comments.map(comment => (
-            <div className="comment">
-                <img src={comment.profilePicture} alt=""/>
-                <div className="info">
-                    <span>{comment.name}</span>
-                    <p>{comment.desc}</p>
-                </div>
-                <div className="date">1 hour ago</div>
+        {isError
+          ? "Something went wrong!"
+          : isLoading
+          ? "Loading..."
+          : (data ?? []).map((comment) => (
+            <div className="comment" key={comment.id}>
+              <img src={comment.profilePicture} alt=""/>
+              <div className="info">
+                <span>{comment.name}</span>
+                <p>{comment.desc}</p>
+              </div>
+              <div className="date"> {moment(comment.createdAt).fromNow()} </div>
             </div>
-        ))}
+          ))}
     </div>
   )
 }
