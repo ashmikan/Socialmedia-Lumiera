1338d29f090a307514653a43f89c311e0ec89010
ashmikan <ashmika.nathali123@gmail.com>
2025-12-12 20:35:13 +0530

    update getPosts query to filter by userId (users have only their posts in profile)

--- frontend/src/components/posts/Posts.jsx
+++ frontend/src/components/posts/Posts.jsx
@@ -3,12 +3,12 @@ import Post from "../post/Post";
 import { useQuery } from '@tanstack/react-query';
 import { makeRequest } from "../../axios";
 
-const Posts = () => {
+const Posts = ({userId}) => {
 
   const { isLoading, error, data } = useQuery({
     queryKey: ["posts"],
     queryFn: () =>
-      makeRequest.get("/posts").then((res) => {
+      makeRequest.get("/posts?userId="+userId).then((res) => {
         return res.data;
       })
   });
--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -107,7 +107,7 @@ const Profile = () => {
             <MoreVertIcon />
           </div>
         </div>
-      <Posts/>
+      <Posts userId={userId} />
       </div>
     </div>
   )
